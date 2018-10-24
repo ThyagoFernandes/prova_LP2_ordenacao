bb1f569d7ab91790cc48341ae7b785c1723d9144
thyagofernandes <thyagomau2@gmail.com>
2018-10-24 18:20:07 -0300

    corrigindopara versão que deveria ser upada de inicio

--- sort.py
+++ sort.py
@@ -1,7 +1,8 @@
 from contextlib import redirect_stdout
-
+import locale
+locale.setlocale(locale.LC_ALL,"")
 with open("rotulos.in", encoding="utf-8") as f:
-    ordered = sorted([line.strip() for line in f.readlines()])
+	ordered = sorted([line.strip() for line in f.readlines()],key= locale.strxfrm)
 
 with open("rotulos.out", "w", encoding="utf-8") as f:
     with redirect_stdout(f):
